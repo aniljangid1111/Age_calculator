b8e3f56a50ab7dcfcd5b700a5a9933ad6cf651a4
aniljangid1111 <aniljangid6199@gmail.com>
2025-05-07 23:24:43 +0530

    try

--- JS/app.js
+++ JS/app.js
@@ -8,14 +8,10 @@ function calAge() {
     let minutesUser = document.querySelector("#minutes");
     let secondsUser = document.querySelector("#seconds");
 
-
-
     let currentTime = new Date();
     // let userTimeZone = new Date(userdata + " " + userTime);
     // let userTimeZone = new Date(userdata + " " + userTime);
     let userTimeZone = new Date(userdata + "T" + (userTime || "00:00"));
-
-
 
     if (!userdata) {
         alert("Please enter your birth date.");
@@ -24,17 +20,9 @@ function calAge() {
 
     let timeDifference = currentTime - userTimeZone
 
-
-
-
-
-
-
     let years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365.25));
     let months = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
     let days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
-    // let days = Math.floor((timeDifference % (1000 * 60 * 60 * 24 * 365.25) - (months * (1000 * 60 * 60 * 24 * 30.44))) / (1000 * 60 * 60 * 24));
-
 
     let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let min = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
